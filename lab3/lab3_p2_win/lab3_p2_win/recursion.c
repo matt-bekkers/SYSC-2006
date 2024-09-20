ea8cba733e67c455ba08bfaa7e0d4994db2ff070
Quench <matthedeb@hotmail.com>
2024-09-19 22:49:22 -0400

    full completed lab 3 part 2 and associated files

---
+++ lab3/lab3_p2_win/lab3_p2_win/recursion.c
@@ -0,0 +1,20 @@
+/* SYSC 2006 Lab 3, part 2. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "recursion.h"
+
+
+
+/* Exercise 4. Return the number of digits in integer n, n >= 0. */
+int num_digits(int n)
+{
+    return -1;
+}
+
+/* Exercise 5. Return x raised to the power n for n >= 0. */
+double power2(double x, int n){
+    return -1;
+}
+
