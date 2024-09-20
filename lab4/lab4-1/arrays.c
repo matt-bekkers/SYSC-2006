9c2f5926ea0210061792679f1fe2b06ee1461df2
Quench <matthedeb@hotmail.com>
2024-09-19 22:53:21 -0400

    initial commit of untouched files

---
+++ lab4/lab4-1/arrays.c
@@ -0,0 +1,62 @@
+/* SYSC 2006 Lab 4. 
+
+ * Incomplete implementations of the functions that will be coded during the lab.
+ */
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+#include "arrays.h"
+
+/* Exercise 1. */
+
+/* Return the average magnitude of the signal represented by
+ * the n doubles in x[]. 
+ * This function assumes that parameter n is >= 1.
+ */
+double avg_magnitude(double x[], int n)
+{
+    return -1.0;
+}
+
+/* Exercise 2. */
+
+/* Return the average power of the signal represented by
+ * the n doubles in x[].
+ * This function assumes that parameter n is >= 1.
+ */
+double avg_power(double x[], int n)
+{
+    return -1.0;
+}
+
+/* Exercise 3. */
+
+/* Return the largest of the n doubles in arr[]. 
+ * This function assumes that parameter n is >= 1.
+ */
+double maxi(double arr[], int n)
+{
+    return -1.0;
+}
+
+/* Exercise 4. */
+
+/* Return the smallest of the n doubles in arr[]. 
+ * This function assumes that parameter n is >= 1.
+ */
+double mini(double arr[], int n)
+{
+    return -1.0;
+}
+
+/* Exercise 5. */
+
+/* Normalize the n doubles in x[]. 
+ * This function assumes that parameter n is >= 2, and that at least
+ * two of the values in x[] are different.
+ */
+void normalize(double x[], int n)
+{
+}
